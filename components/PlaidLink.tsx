e018853663cfb74bda451d082276854cc35bcbcc
Danny Li <dannybgy@gmail.com>
2024-09-25 23:46:21 -0400

    implement PlaidLink and Dwolla

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -6,7 +6,10 @@ import {
   usePlaidLink,
 } from 'react-plaid-link';
 import { useRouter } from 'next/navigation';
-import { createLinkToken } from '@/lib/actions/user.action';
+import {
+  createLinkToken,
+  exchangePublicToken,
+} from '@/lib/actions/user.action';
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
   const router = useRouter();
@@ -22,10 +25,10 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
   }, [user]);
   const onSuccess = useCallback<PlaidLinkOnSuccess>(
     async (public_token: string) => {
-      // await exchangePublicToken({
-      //     publicToken: public_token,
-      //     user,
-      // })
+      await exchangePublicToken({
+        publicToken: public_token,
+        user,
+      });
 
       router.push('/');
     },
